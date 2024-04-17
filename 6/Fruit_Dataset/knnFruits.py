4afa03a3327326559d625c26328344eecf3589c9
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2024-04-17 14:58:52 +0530

    Update knnFruits.py

--- 6/Fruit_Dataset/knnFruits.py
+++ 6/Fruit_Dataset/knnFruits.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-# for data manipulation, analysis and reading our dataset
 import pandas as pd
 
-# for plotting and visualising the data
 import matplotlib.pyplot as plt
 fruit = pd.read_csv('fruit_data_with_colours.csv')
 import seaborn as sns
@@ -16,8 +14,8 @@ import seaborn as sns
 import pylab as pl
 sns.countplot(fruit['fruit_name'], label="Count", palette="Set3")
 plt.show()
-fruit.drop('fruit_label', axis=1).plot(kind='box', subplots=True, layout=(2,2), 
-                                        sharex=False, sharey=False, figsize=(10,10), 
+fruit.drop('fruit_label', axis=1).plot(kind='box', subplots=True, layout=(2,2),
+                                        sharex=False, sharey=False, figsize=(10,10),
                                         color ='c', patch_artist=True)
 
 
@@ -25,11 +23,9 @@ pl.suptitle("Box Plot for each input variable")
 plt.savefig('fruits_boxplot')
 plt.show()
 
-# PyLab is a module that belongs to the Python mathematics library Matplotlib. 
-# PyLab combines the numerical module numpy with the graphical plotting module pyplot
 
 
-# To create a histogram, we will use pandas hist() method.
+
 fruit.drop('fruit_label', axis=1).hist(bins=30, figsize=(10,10), color = "c", ec = "m", lw=0)
 pl.suptitle("Histogram for each numeric input variable")
 plt.savefig('fruits_histogram')
@@ -45,28 +41,20 @@ scatter_matrix(df, alpha=0.2, cmap = cmap, figsize=(10,10), marker = '.', s=30, 
 plt.suptitle('Scatter-matrix for each input variable')
 plt.savefig('fruit_scatter_matrix')
 
-# First, import the KNeighborsClassifier module
-# details about the module here: https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+
 from sklearn.neighbors import KNeighborsClassifier
 
-# In order to understand the model performance, divide the dataset into a training set and a test set.
-# The split is done by using the function train_test_split()
-# details here: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+
 from sklearn.model_selection import train_test_split
 
 
-# Split the dataset into two different datasets
-# X for the independent features such as mass, width, height
-# Y for the dependent feature i.e. fruit name
+
 X = fruit[['mass','width','height','color_score']]
 Y = fruit['fruit_name']
 
-# Now split the dataset X into two separate sets — X_train and X_test 
-# Similarly, split the dataset Y into two sets — y_train and y_test
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
-# Notice the use of test_size. This parameter decides the size of the data that has to be split as the test dataset
-# In the above case it is 0.2, which means that the dataset will be split 20% as the test dataset
 
 X_train.describe()
 
@@ -75,12 +63,10 @@ X_test.describe()
 
 knn = KNeighborsClassifier()
 
-# Train the model using the training sets
+
 knn.fit(X_train, y_train)
 
-# Model Accuracy, how often is the classifier correct?
-# Accuracy can be computed by comparing actual test set values and predicted values.
-# The score function is simply a utility function for a default metric to be used within some algorithms of scikit-learn
+
 knn.score(X_test, y_test)
 print("Accuracy for K=5 : ", knn.score(X_test, y_test))
 
@@ -126,37 +112,31 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 import matplotlib.patches as mpatches
 from sklearn import neighbors, datasets
 
-# import some data to play with
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features. We could avoid this ugly slicing by using a two-dim dataset
+X = iris.data[:, :2] 
 y = iris.target
-h = .02  # step size in the mesh
+h = .02 
 
 n_neighbors = 7
 
-# Create color maps
 cmap_light = ListedColormap(['#FFFACD', '#7FFFD4', '#87CEFA'])
 cmap_bold = ListedColormap(['#FF0000', '#228B22', '#0000FF'])
 
 for weights in ['distance']:
-    # we create an instance of Neighbours Classifier and fit the data.
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
     clf.fit(X, y)
 
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, x_max]x[y_min, y_max].
+ 
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
-    # Put the result into a color plot
     Z = Z.reshape(xx.shape)
     plt.figure()
     plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
-    # Plot also the training points
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
